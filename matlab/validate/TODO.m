aeb3da12df9b391a0fc5dbeffaa8c62a754fa1eb
Brian Wandell <wandell@stanford.edu>
2016-03-08 13:22:16 -0800

    Aspirations

---
+++ matlab/validate/TODO.m
@@ -0,0 +1,37 @@
+%% Validation scripts we hope to write
+%
+% The scitranClient utilities are designed to interact with the scitran API
+% that manages MR and related neuroimaging data.
+%
+% The utlities require user authentication to interact with the database.
+% The basic capabilities we plan are simply for
+%
+%    * Search
+%    * Download
+%    * Upload
+%
+% We wil build utilities on top of those capabilities over time.
+% What will be returned will be a mix of URLs to the data or in some cases
+% the data themselves.
+% The search will extend (a) through MongoDB only? or (b) through the
+% Elastic search capabilities.
+%
+% Examples
+%   Return all the diffusion scans for males between 12 and 15 years of age
+%   Return the T1 scans for people in the ADNI project
+%
+% 
+%
+% LMP/BW Scitran Team, 2016
+
+%% Check that the database is up
+
+%% Authorization
+
+%% Does a search
+
+%% Does a download
+
+%% Does an upload
+
+%%
