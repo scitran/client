fcf74d2acb312977fc48fe280e046bb8db4c9654
Brian Wandell <wandell@stanford.edu>
2016-04-07 18:23:52 -0700

    Comments, neatening.

--- matlab/validate/v_sdmQuery.m
+++ matlab/validate/v_sdmQuery.m
@@ -17,6 +17,14 @@
 % You should insert the string "_0x2E_".   Because this is ugly, we might
 % use the syntax _dot_ and then do a strrep()
 %
+% Notes on curl configuration
+%
+% On your system, you must have curl libraries properly configured.  Matlab
+% has its own environment
+%
+%    cENV = configure_curl;
+%       ... Your code here ...
+%    unconfigure_curl(cENV);
 %   
 % LMP/BW Scitran Team, 2016
 
@@ -37,18 +45,10 @@ warning('off', 'MATLAB:namelengthmaxexceeded');
 
 %% Does a search
 
-% Now, we can search for elements of the data model
-%
-%    https://github.com/scitran/core/wiki/Data-Model,-v2
-%
-
 % The main information types for the scitran client are
 %
 %    Group, Project, Session, Acquisition and Files
 %
-% There are other types that are used internally for the data model.
-%
-
 %% To search for subjects within that age and of a particular sex
 % srch = loadjson('sdm_search1.json');
 % 
@@ -94,43 +94,51 @@ s.url    = furl;
 s.token  = token;
 s.body   = jsonData;
 s.target = 'files';
-s.collection = 'patient';
+s.collection = 'patients';
 srchCMD = sdmCommandCreate(s);
 
 %%
 [~, result] = system(srchCMD);
-
-% Functionalize this
-% result = sdmSearch(srchCMD);
-% [status, result] = system(srchCMD);
-
-%% Now start parsing the json result that was returned
-
-
-% syscommand = sdmCommandCreate('url',furl,'token',token,'body',jsonData,'target','session');
-% sdmCommandRun(curcmd,jsonData)
-
-
-% On your system, you must have curl libraries properly configured
-%cENV = configure_curl;
-%[status, result] = system(syscommand);
-%unconfigure_curl(cENV);
-
-% Dump the data
 scitranData = loadjson(result);
+scitranData{1}
+
+
+%% Dump the data names
 for ii=1:length(scitranData)
     scitranData{ii}.type
     scitranData{ii}.name
 end
 
+%%
+clear jsonSend
+jsonSend.multi_match.fields = '*';
+jsonSend.multi_match.query = '.zip';
+jsonSend.multi_match.lenient = 'true';
 
+% Convert
+jsonData = savejson('',jsonSend);
 
+% Build up the curl command
+clear s
+s.url    = furl;
+s.token  = token;
+s.body   = jsonData;
+s.target = 'files';
+s.collection = 'patients';
+[~, result] = system(sdmCommandCreate(s));
+
+% Dump the data names
+scitranData = loadjson(result);
+for ii=1:length(scitranData)
+    scitranData{ii}.type
+    scitranData{ii}.name
+end
 %% Does a download
 
 %% Does an upload
 
 %%
 
-unconfigure_curl(cENV);
+
 
 %%
