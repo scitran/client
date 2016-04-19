7073906573015f60aff816e33a0d7eb77efbc796
Brian Wandell <wandell@stanford.edu>
2016-04-18 17:58:19 -0700

    Nothing important.  Just some comments, preparing for publishing a pdf and uploading it, too.

--- matlab/stDirCreate.m
+++ matlab/stDirCreate.m
@@ -3,6 +3,7 @@ function stDirCreate(d)
 %
 % LMP,BW Vistasoft Team, 2016
 
+% Check for exitence.  If there, empty it.  If not there, create it.
 if exist(d,'dir'),     delete(fullfile(d,'*'))
 else                   mkdir(d);
 end
