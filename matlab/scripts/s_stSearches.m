081b31ff5b5c464f72d1d99327f62efbbc3b3b79
Brian Wandell <wandell@stanford.edu>
2016-05-06 23:06:27 -0700

    Moved files around.  Need to man up and delete some.

--- matlab/scripts/s_stSearches.m
+++ matlab/scripts/s_stSearches.m
@@ -102,7 +102,7 @@
 clear b
 b.path = 'projects';
 s.json = b;
-projects = stEsearchRun(s);
+projects = stSearch(s);
 
 fprintf('Found %d projects\n',length(projects))
 
@@ -116,7 +116,7 @@ b.projects.bool.must(2).match.label = 'vwfa';
 
 % b.projects.match.label = 'vwfa'
 s.json = b;
-projects = stEsearchRun(s);
+projects = stSearch(s);
 fprintf('Found %d projects for the group wandell, with label vwfa.\n',length(projects))
 
 % Save this project information
@@ -132,7 +132,7 @@ clear b
 b.path = 'sessions';
 b.projects.match.x0x5F_id = projectID;   % Note the ugly x0x5F.  See notes.
 s.json = b;
-sessions = stEsearchRun(s);
+sessions = stSearch(s);
 fprintf('Found %d sessions in the project %s\n',length(sessions),projectLabel);
 
 % Save this session information
@@ -148,7 +148,7 @@ clear b
 b.path = 'acquisitions';
 b.sessions.match.x0x5F_id = sessionID;
 s.json = b;
-acquisitions = stEsearchRun(s);
+acquisitions = stSearch(s);
 fprintf('Found %d acquisitions in the session %s\n',length(acquisitions),sessionLabel);
 
 % This brings up the session containing this acquisition
@@ -161,7 +161,7 @@ b.path = 'files';
 b.sessions.match.x0x5F_id = sessionID;
 b.files.match.type = 'nifti';
 s.json = b;
-files = stEsearchRun(s);
+files = stSearch(s);
 
 nFiles = length(files);
 fprintf('Found %d nifti files in the session %s\n',nFiles,sessionLabel);
@@ -180,21 +180,21 @@ clear b
 b.path = 'analyses';
 b.collections.match.label = 'GearTest';
 s.json = b;
-analyses = stEsearchRun(s);
+analyses = stSearch(s);
 
 % Which collection is the analysis in?
 clear b; 
 b.path = 'collections';
 b.collections.match.label = 'GearTest';
 s.json = b;
-collections = stEsearchRun(s);
+collections = stSearch(s);
 
 % Find a session from that collection
 clear b; 
 b.path = 'sessions'; 
 b.collections.match.label = 'GearTest';
 s.json = b;
-sessions = stEsearchRun(s);
+sessions = stSearch(s);
 
 % Bring up the browser to that collection and session
 stBrowser(s.url,sessions{1},'collection',collections{1})
@@ -205,7 +205,7 @@ clear b
 b.path = 'collections';
 b.sessions.range.created.gte = 'now-4w';  % For weeks ago
 s.json = b;
-collections = stEsearchRun(s);
+collections = stSearch(s);
 fprintf('Found %d collections in previous two weeks \n',length(collections))
 
 % To see the sessions within some time range use:
@@ -222,7 +222,7 @@ b.path = 'sessions';
 subjectCode = 'ex4842';
 b.sessions.match.subject_0x2E_code = subjectCode;
 s.json = b;
-sessions = stEsearchRun(s);
+sessions = stSearch(s);
 fprintf('Found %d sessions with subject code %s\n',length(sessions),subjectCode)
 
 % Click to 'Subject' tab to see the subject code
@@ -235,7 +235,7 @@ b.path = 'sessions';
 b.sessions.bool.must{1}.range.subject_0x2E_age.gt = year2sec(10);
 b.sessions.bool.must{1}.range.subject_0x2E_age.lt = year2sec(15);
 s.json = b;
-sessions = stEsearchRun(s);
+sessions = stSearch(s);
 % savejson('',s.json)
 
 fprintf('Found %d sessions\n',length(sessions))
@@ -251,7 +251,7 @@ clear b
 b.path = 'sessions';
 b.sessions.match.label= sessionLabel;
 s.json = b;
-sessions = stEsearchRun(s);
+sessions = stSearch(s);
 fprintf('Found %d sessions with the label %s\n',length(sessions),sessionLabel)
 
 % Browse the session.
@@ -266,7 +266,7 @@ b.sessions.match.label = '20120522_1043';
 b.acquisitions.match.label = '11_1_spiral_high_res_fieldmap';
 b.files.match.type = 'nifti';
 s.json = b;
-files = stEsearchRun(s);
+files = stSearch(s);
 fprintf('Found %d matches to this files label\n',length(files));
 
 % This is how to download the nifti file
@@ -283,7 +283,7 @@ b.acquisitions.match.label = 'SPGR 1mm 30deg';
 b.files.match.type = 'nifti';
 s.json = b;
 
-files = stEsearchRun(s);
+files = stSearch(s);
 
 %%
 clear b
@@ -293,7 +293,7 @@ b.acquisitions.match.measurement = 'Diffusion';
 b.files.match.type = 'bvec';
 
 s.json = b;
-files = stEsearchRun(s);
+files = stSearch(s);
 
 %% get files in project/session/acquisition/collection
 
@@ -302,7 +302,7 @@ b.path = 'files';                         % Looking for T1 weighted files
 b.collections.match.label = 'ENGAGE';     % Collection is ENGAGE
 b.acquisitions.match.label = 'T1w 1mm';   % Description column
 s.json = b;
-files = stEsearchRun(s);
+files = stSearch(s);
 fprintf('Found %d matching files\n',length(files))
 
 %% get files from a collection
@@ -314,7 +314,7 @@ b.acquisitions.match.label = 'SPGR 1mm 30deg';
 b.files.match.name = '16.1_dicom_nifti.nii.gz';
 s.json = b;
 
-files = stEsearchRun(s);
+files = stSearch(s);
 fprintf('Found %d matching files\n',length(files))
 
 for ii=1:length(files)
