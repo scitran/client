555fef0159f82178e352b620fdd0b3971ba1e3e4
Brian Wandell <wandell@stanford.edu>
2016-05-08 21:51:45 -0700

    Fixed for new format

--- matlab/scripts/s_stSummary.m
+++ matlab/scripts/s_stSummary.m
@@ -10,36 +10,26 @@
 %% Authorization
 
 % The auth returns a token and the url of the flywheel instance
-[token, furl, ~] = stAuth('action', 'create', 'instance', 'scitran');
-fprintf('Token length %d\nConnected to: %s\n',length(token),furl)
-
-% We use the structure s to store the parameters needed to create the
-% elastic search command.  The url and token are fixed throughout, so we
-% set them here.  
-
-clear s
-s.url    = furl;
-s.token  = token;
+st = scitran('action', 'create', 'instance', 'scitran');
 
 %%  Start building up string
 
-str = sprintf('Summary for site %s\n\n',s.url);
+str = sprintf('Summary for site %s\n\n',st.url);
 
-clear b
-b.path = 'projects';
-s.json = b;
-projects = stEsearchRun(s);
+clear srch
+srch.path = 'projects';
+projects = st.search(srch);
 str =  addText(str,sprintf('There are %d projects\n',length(projects)));
 
 %%
 nSessions = 0;
 for ii=1:length(projects)
-    clear b
-    b.path = 'sessions';
+    clear srch
+    srch.path = 'sessions';
     pLabel = projects{ii}.source.label;
-    b.projects.match.label = projects{ii}.source.label;
-    s.json = b;
-    sessions = stEsearchRun(s);
+    srch.projects.match.label = projects{ii}.source.label;
+    sessions = st.search(srch);
+    
     nSessions = nSessions + length(sessions); 
     str2 = sprintf('\t%s (%d sessions)\n',pLabel,length(sessions))
     str  =  addText(str,str2);
