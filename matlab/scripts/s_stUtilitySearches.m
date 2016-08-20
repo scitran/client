8e1e71e5198a057950d664aaf73396ff4c1a8cd5
Brian Wandell <wandell@stanford.edu>
2016-08-19 18:07:43 -0700

    New utilities for complex searches ... RF and me looking for useful functionality

--- matlab/scripts/s_stUtilitySearches.m
+++ matlab/scripts/s_stUtilitySearches.m
@@ -5,23 +5,100 @@
 %
 % RF/BW Vistasoft Team 2016
 
-% Get authorization to read from the database
+%% Get authorization to read from the database
 st = scitran('action', 'create', 'instance', 'scitran');
 
 % A place for temporary files.
 chdir(fullfile(stRootPath,'local'));
 
-%% Find analyses
+%% How many total sessions in a project?
+
+projectName = 'ENGAGE';
 
 clear srch
 srch.path = 'sessions';
-srch.projects.match.label = 'svip';
-srch.sessions.match.analyses_0x2E_label = 'fsl-bet';
+srch.projects.match.label = projectName;
+allSessions = st.search(srch);
 
-[s,~,cmd] = st.search(srch);
+% Which ones ran fsl-bet and which did not?
+analysisName = 'fsl-bet';
+srch.sessions.match.analyses_0x2E_label = analysisName;
 
-%%
-st.browser(s{1})
+analyzedSessions = st.search(srch);
+fprintf('Found %d sessions out of %d with analysis: %s (project: %s)\n',...
+    length(analyzedSessions),length(allSessions), analysisName,projectName);
+
+%% Find the users who ran this analysis
+fprintf('These were run by:\n\n');
+for ii=1:length(analyzedS)
+    analyses = analyzedSessions{ii}.source.analyses;
+    for jj=1:length(analyses)
+        fprintf('Analysis: %s \tUser:  %s\n',analyses{jj}.label,analyses{jj}.user);
+    end
+end
+
+%% Look through the whole database for analyses using fsl-bet
+
+clear srch
+srch.path = 'sessions';
+analysisName = 'fsl-bet';
+srch.sessions.match.analyses_0x2E_label = analysisName;
+analyzedSessions = st.search(srch,'all_data',true);
+
+fprintf('Found %d sessions  analysis: %s (all data)\n',...
+    length(analyzedSessions), analysisName);
+
+fprintf('These sessions include an fsl-bet:\n\n');
+for ii=1:length(analyzedSessions)
+    fprintf('Session: %s\n----\n',analyzedSessions{ii}.source.label);
+    analyses = analyzedSessions{ii}.source.analyses;
+    for jj=1:length(analyses)
+        fprintf('Analysis: %s \tUser:  %s\n',analyses{jj}.label,analyses{jj}.user);
+    end
+    fprintf('\n\n');
+end
+
+%% Find session in ENGAGE that have an Anatomy acquisition
+
+projectName = 'ENGAGE';
+
+clear srch
+srch.path = 'sessions';
+srch.projects.match.label = projectName;
+% Which ones ran fsl-bet and which did not?
+% analysisName = 'fsl-bet';
+% srch.sessions.match.analyses_0x2E_label = analysisName;
+measurement = 'Anatomy';
+srch.acquisitions.match.measurement = measurement;
+
+analyzedSessions = st.search(srch);
+
+for ii=1:length(analyzedSessions)
+    clear srch
+    srch.path = 'analyses';
+    srch.sessions.match.label = analyzedSessions{ii}.source.label;
+    analyses = st.search(srch);
+    if isempty(analyses)
+        disp('Run fsl-bet')
+    else
+        disp('Checking if fsl-bet is an analysis')
+    end
+end
+
+
+%% Find the collections that contain a certain analyses
+
+clear srch
+srch.path = 'collections/analyses';
+srch.projects.match.label = projectName;
+srch.sessions.match.analyses_0x2E_label = analysisName;
+analyzedS = st.search(srch);
+
+fprintf('%d instances of the analysis: %s were found\n',length(analyzedS),analysisName);
+fprintf('These were run by:\n\n');
+for ii=1:length(analyzedS)
+    fprintf('User:  %s in collection: %s\n',analyzedS{ii}.source.user,analyzedS{ii}.source.container.label);
+end
 
 %%  Find all the sessions with fsl-bet
 
