f424b87bce38b151c2216710f2bbeae10b483e1e
Brian Wandell <wandell@stanford.edu>
2017-02-12 15:25:15 -0800

    We can now download fw_Apricot6.m and execute it locally. Let's add more features, checks, and such.

--- matlab/@scitran/runScript.m
+++ matlab/@scitran/runScript.m
@@ -1,8 +1,29 @@
-function runScript(stClient,scriptName)
+function execFileName = runScript(stClient,scriptName,varargin)
 % Search, download, and run a script stored as an attachment at Flywheel
 %
-%    stClient = scitran('action', 'create', 'instance', 'scitran');
-%    stClient.runScript('fw_Apricot6.m')
+% The scriptName should be stored on the Flywheel instance,k and it should
+% conform to specifications that we (will) write.  
+%
+% Required inputs: 
+%   scriptName - Filename of the script 
+%
+% Optional inputs (to help find the script)
+%   destinationDir - Directory to place the scriptName
+%   projectid  - Specify search parameter srch.project.match.name
+%   sessionid  - Specify search parameter srch.session.match.name
+%   doRun      - Boolean specifying whether to execute or not (default = true)
+%
+% Return:
+%   execFileName   - Full path to the local copy of the script name  
+%
+% Example
+%  stClient = scitran('action', 'create', 'instance', 'scitran');
+%  stClient.runScript('fw_Apricot6.m')
+%  stClient.runScript('fw_Apricot6.m','destinationDir',fullfile(stRootPath,'local'))
+%  eFile = stClient.runScript('fw_Apricot6.m','doRun',false)
+%
+% The script you execute should comply with the specifications in
+%    ** In which our heros write some specifications **
 %
 % BW/RF
 
@@ -10,11 +31,32 @@ function runScript(stClient,scriptName)
 p = inputParser;
 p.addRequired('scriptName');
 
+% Specify a directory that will be used as the folder for the script.  The
+% default is a random name in the tmp dir for this system.
+p.addParameter('destinationDir',tempname,@ischar);
+p.addParameter('doRun',true,@islogical);
+
+p.addParameter('sessionid',[],@ischar);
+p.addParameter('projectid',[],@ischar);
+
+p.parse(scriptName,varargin{:});
+
+destinationDir = p.Results.destinationDir;
+doRun = p.Results.doRun;
+
 %% Search and download the script
-%
+
+% Set up the search variable.  It always returns a single file
 clear srch
 srch.path = 'files';
-srch.files.match.name = scriptName;  % FOr testing scriptName = 'fw_Apricot6.m'
+
+% For testing use
+%   scriptName = 'fw_Apricot6.m'
+srch.files.match.name = scriptName;
+
+% Put in the session and project ids here, when we are ready
+
+% Do the search
 files = stClient.search(srch);
 if numel(files) ~= 1
     error('Wrong number of files returned: %d\n',numel(files));
@@ -27,13 +69,16 @@ end
 
 % This won't run until the Issue is fixed by Renzo for building the
 % permalink of a file.
-execFileName = stClient.get(files{1});
+if ~exist(destinationDir,'dir'), mkdir(destinationDir); end
+execFileName = stClient.get(files{1},'destination',fullfile(destinationDir,scriptName));
 
-%% Update any github repos and check path
+%% Update github repos, check path
 
-%% Execute the script
-run(execFileName);
 
+%% Execute the script (locally)
+if doRun, run(execFileName); end
+
+end
 %%
 
 
