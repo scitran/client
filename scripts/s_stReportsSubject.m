60e7b17e2c7cd8d268e3ee17d20c28929a103ac7
Brian Wandell <wandell@stanford.edu>
2017-04-18 07:02:01 -0700

    updated script for showing Matlab interaction with Flywheel

--- scripts/s_stReportsSubject.m
+++ scripts/s_stReportsSubject.m
@@ -1,94 +1,64 @@
-%% Scitran project searches
+%% Scitran project reporting
 %
+%  Illustrates searching the Flywheel database from Matlab for subject
+%  information in a particular project.  This code works for scitran
+%  generally, though I prefer working with the Flywheel instance.
 %
-
-%%
-close all
+% BW/Scitran Team 2016
 
 %% Authorization
 
-% The auth returns a token and the url of the flywheel instance.  These are
-% fixed as part of 's' throughout the examples, below.
+% Open the scitran object
 st = scitran('action', 'create', 'instance', 'scitran');
 
-%%  List all the projects
+%% Find the project of interest
 
 clear srch
 srch.path = 'projects';
 projects = st.search(srch);
-
-nProjects = length(projects);
-Project = (1:nProjects)';
-Name = cell(nProjects,1);
-for ii = 1:nProjects
-    Name{ii} = projects{ii}.source.label;
+for ii=1:length(Name)
+    if isequal(projects{ii}.source.label,'SVIP Released Data (SIEMENS)')
+        pp = ii;
+        break;
+    end
 end
-T = table(Project,Name);
-disp(T)
 
-%% Characterize the subject ages
+%% Search for the subject information
 
-pp = 4;  % Pick project
-
+% Find the sessions in the project
 clear srch
 srch.path = 'sessions';
 srch.projects.match.exact_label = Name{pp};
 sessions = st.search(srch);
 length(sessions)
 
+% Get the subject information
 subjects = stSubjectInfo(sessions);
-ages = stSubjectGet(subjects,'age');
+ages     = stSubjectGet(subjects,'age');
 
-fprintf('Subjects with unknown ages %d\n',sum(ages==0));
-
-figure; hist(ages,20); xlabel('Age in years'); ylabel('N subjects');
+% Summarize ages in a graph
+stNewGraphWin; hist(ages,20); xlabel('Age in years'); ylabel('N subjects');
 set(gca,'xlim',[0 100]);
 title(sprintf('Project: %s',Name{pp}));
 
-%% Characterize the sex distribution
+%% Print out the sex distribution
 
 sex = stSubjectGet(subjects,'sex');
-
-fprintf('\n---------\n');
-fprintf('Sessions with unknown subject sex:  %d\n',find(sex == 'u'));
-fprintf('---------\n');
+unknownSexSessions = find(sex == 'u');
+if isempty(unknownSexSessions)
+    fprintf('\n---------\n');
+    fprintf('Sex coded in all sessions\n');
+    fprintf('---------\n');
+else
+    fprintf('\n---------\n');
+    fprintf('Sessions with unknown subject sex:  %d\n',unknownSexSessions);
+    fprintf('---------\n');
+end
 
 nMale    = sum(sex=='m');
 nFemale  = sum(sex =='f');
 nUnknown = sum(sex == 'u');
-fprintf('%d Males\n%d Females\n%d Unknown\n',nMale,nFemale,nUnknown);
-
-%%  Find the sessions in the SVIP Released project
-
-clear srch
-srch.path = 'sessions';
-srch.projects.match.label = 'SVIP Released';
-sessions = st.search(srch);
-fprintf('Number of sessions:  %d\n',length(sessions))
-
-% Get the subject information from those sessions
-subjects = stSubjectInfo(sessions);
-
-% Specifically the ages
-ages = stSubjectGet(subjects,'age');
-
-% Summarize
-fprintf('Total subjects %d  (unknown ages %d)\n',length(subjects),sum(ages==0));
-
-% Plot
-stNewGraphWin('color',[0.8 0.8 0.8]); 
-hist(ages,20); xlabel('Age in years'); ylabel('N subjects');
-set(gca,'xlim',[0 100]);
-title(sprintf('Project: %s',srch.projects.match.label));
-
-sex = stSubjectGet(subjects,'sex');
-
-fprintf('\n---------\n');
-nMale    = sum(sex=='m');
-nFemale  = sum(sex =='f');
-nUnknown = sum(sex == 'u');
-fprintf('%d Males\n%d Females\n%d Unknown\n',nMale,nFemale,nUnknown);
-fprintf('---------\n');
+fprintf('%d Males\n%d Females\n%d   Unknown\n',nMale,nFemale,nUnknown);
 
 %%
 
