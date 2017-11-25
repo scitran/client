187e5a74839f0f47e025756470d36c353bd7e00a
Brian Wandell <wandell@stanford.edu>
2017-11-24 21:42:49 -0800

    Made dwiLoad conform with scitran download routine. Got rid of collection from search.  Collection is not yet understood. update changed to modify to be consistent with Flywheel naming.

--- @scitran/dwiLoad.m
+++ @scitran/dwiLoad.m
@@ -1,15 +1,14 @@
 function [dwi,destination] = dwiLoad(st,acquisitionID,varargin)
-% Load nifti, bvec, and bval from a flywheel acquisition
+% Load nifti, bvec, and bval from a Flywheel acquisition
 %
-%      dwi = st.dwiLoad(acquisitionID,varargin);
-%      dwi = dwiLoad(st,acquisitionID,varargin);
+% Syntax
+%   dwi = st.dwiLoad(acquisitionID,varargin);
 %
-% We assume there is one nifti, bval, and bvec file in the acquisition.
-% These three files are downloaded and their values are returned as a dwi
-% struct that can be treated as per vistasoft (dwiGet/Set)
+% There must be one nifti, bval, and bvec file in the acquisition. These
+% three files are downloaded and their values are returned as a dwi struct
+% that can be treated as per vistasoft (dwiGet/Set)
 %
 % Required inputs
-%  st:            A scitran object
 %  acquistionID:  Flywheel acquisition id
 % 
 % Optional inputs:
@@ -17,16 +16,29 @@ function [dwi,destination] = dwiLoad(st,acquisitionID,varargin)
 %                 no directory is specified, they are written into
 %                 scitranClient/local. 
 %
-% Example:
-%   See s_stDiffusion.m, s_stALDIT.m
+% See also: s_stDiffusion.m, s_stALDIT.m
 %
 % BW Scitran Team, 2017
+
+%{
+ % Example:
+ project = 'ALDIT';
+ session = 'Set 3';
+ acquisitions = st.search('acquisition', ...
+            'project label exact',project,...
+            'session label exact',session,...
+            'acquisition label contains','1000');
+ dwi = st.dwiLoad(acquisitions{1}.acquisition.x_id);
+%}
+
 
 %% Parse
 p = inputParser;
 p.addRequired('acquisitionID',@ischar);
 p.addParameter('destination',fullfile(stRootPath,'local'),@ischar);
+
 p.parse(acquisitionID,varargin{:});
+
 destination = p.Results.destination;
 if ~exist(destination,'dir'), mkdir(destination); end
 
@@ -39,21 +51,21 @@ bvecFile = st.search('files',...
     'file type','bvec');
 if length(bvecFile) > 1, warning('Multiple bvec files.  Returning 1st'); end
 bvecName = fullfile(destination,bvecFile{1}.file.name);
-st.fw.downloadFileFromAcquisition(acquisitionID,bvecFile{1}.file.name,bvecName);
+st.downloadFile(bvecFile{1},'destination',bvecName);
 
 bvalFile = st.search('files',...
     'acquisition id',acquisitionID,...
     'file type','bval');
 if length(bvalFile) > 1, warning('Multiple bval files.  Returning 1st'); end
 bvalName = fullfile(destination,bvalFile{1}.file.name);
-st.fw.downloadFileFromAcquisition(acquisitionID,bvalFile{1}.file.name,bvalName);
+st.downloadFile(bvalFile{1},'destination',bvalName);
 
 niiFile = st.search('files',...
     'acquisition id',acquisitionID,...
     'file type','nifti');
 if length(niiFile) > 1, warning('Multiple nii files.  Returning 1st'); end
 niiName = fullfile(destination,niiFile{1}.file.name);
-st.fw.downloadFileFromAcquisition(acquisitionID,niiFile{1}.file.name,niiName);
+st.downloadFile(niiFile{1},'destination',niiName);
 
 %% Read and return
 
