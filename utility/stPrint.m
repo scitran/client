e2290796c6c762c7a4f3ac3f3ec073312ac2f7b3
Brian Wandell <wandell@stanford.edu>
2017-12-02 11:04:16 -0800

    Comments

--- utility/stPrint.m
+++ utility/stPrint.m
@@ -1,15 +1,44 @@
 function stPrint(result,slot, field)
 % Print the fields from a result to the command window
 %
+% Syntax 
+%  stPrint(containerStruct, slot, field)
+%
+% Description
+%  Print out a list of the values in a cell array, typically returned from
+%  a search, of the structs defining an object.  The slot refers to the
+%  first struct entry (e.g., session or subject) and the field refers to
+%  the second entry (e.g., label or code or ...).  Print out an object to
+%  see the possibilities.  
 %
 % Example
-%   fw = scitran('vistalab');
-%   projects = fw.search('project');
+%   st = scitran('vistalab');
+%   projects = st.search('project');
 %   stPrint(projects,'project','label');
+%   
+% See more examples in the source code
 %
-% BW, Scitran
-%
+% BW, Vistasoft Team, 2017
 
+% Example
+%{
+
+  % All project labels
+  st = scitran('vistalab');
+  projects = st.search('project');
+  stPrint(projects,'project','label');
+
+  % Limited to make the example short.
+  sessions = st.search('session',...
+      'project id',idGet(projects{1}), ...
+      'limit',10);
+  stPrint(sessions,'session','label');
+
+  stPrint(sessions,'subject','code');
+
+%}
+
+%% Parse
 p = inputParser;
 p.addRequired('result',@iscell);
 p.addRequired('slot',@ischar);
