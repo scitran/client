d455fc1d956fd716786ab2a81b4170feedeba32a
Brian Wandell <wandell@stanford.edu>
2017-12-19 08:48:51 -0800

    Utility to print array of toolbox information

---
+++ utility/tbxPrint.m
@@ -0,0 +1,56 @@
+function tbxPrint(tbx)
+% Print a summary of an array of toolboxes
+%
+% Syntax
+%   tbxPrint(tbx)
+%
+% Description
+%   Reads the fields and prints a convenient summary of the toolbox
+%   specifications.
+%
+% Input (required)
+%  tbx - Array of toolboxes
+% Input (optional)
+%  None
+% Return
+%  None
+%
+% BW, Vistasoft team, 2017
+%
+% See also:  tbxRead, tbxWrite, scitran.toolboxValidate
+
+%{
+tbx = st.getToolbox('aldit-toolboxes.json',...
+    'project','ALDIT');
+tbxPrint(tbx);
+%}
+
+%% 
+p = inputParser;
+p.addRequired('tbx',@(x)(isa(x,'toolboxes')));
+p.parse(tbx);
+
+%%
+nTbx = numel(tbx);
+
+for ii=1:nTbx
+    fprintf('\nToolbox %d\n',ii)
+    fprintf('-------------\n')
+
+    fprintf('Toolbox project name %s\n',tbx(ii).gitrepo.project);
+    fprintf('User account: %s\n',tbx(ii).gitrepo.user);
+    fprintf('Commit:  %s\n',tbx(ii).gitrepo.commit);    
+    url = sprintf('https://github.com/%s/%s',tbx(ii).gitrepo.user,tbx(ii).gitrepo.project); 
+    fprintf('Repository url:  %s\n',url);
+    
+    fprintf('-------------\n')
+    cmd = which(tbx(ii).testcmd);
+    fprintf(' %s',tbx(ii).testcmd);
+    if isempty(cmd), fprintf(' is not on your current path.\n')
+    else
+        repoDirectory = fileparts(cmd);
+        fprintf(' is on your current path in directory %s.\n',repoDirectory);
+    end
+    fprintf('\n\n')
+
+end
