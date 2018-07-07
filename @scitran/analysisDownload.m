c61947a518549ad76c8b7310da002a2666a4346a
Brian Wandell <wandell@stanford.edu>
2018-07-06 23:50:03 -0700

    download a file from the analysis output

--- @scitran/analysisDownload.m
+++ @scitran/analysisDownload.m
@@ -1,13 +1,18 @@
-function thisAnalysis = analysisDownload(obj,id,varargin)
-% Return the information a Flywheel analysis
+function destination = analysisDownload(obj,id,fname,varargin)
+% Return an output file from a Flywheel analysis
 %
-%   thisAnalysis = scitran.analysisDownload(id, ...)
+% Syntax
+%   destination = scitran.analysisDownload(id, ...)
+%
+% Description
+%  Words about analyses
 %
 % Required Inputs
 %  id  - The Flywheel analysis ID
+%  fname - Name of the analysis output file
 %
 % Optional key/value parameters
-%  destination:  full path to file output location (default is a tempdir)
+%  destination:  full path to file output location
 %
 % Return
 %  thisAnalysis:  Full path to the file object on disk
@@ -21,28 +26,65 @@ function thisAnalysis = analysisDownload(obj,id,varargin)
 %{
   st = scitran('stanfordlabs');
   analysis = st.search('analysis',...
-    'project label contains','SOC',...
-    'session label exact','stimuli');
+   'project label exact','Brain Beats',...
+   'session label exact','20180319_1232');
   id = idGet(analysis{1},'data type','analysis');
-  thisAnalysis = st.analysisInfoGet(id);  
+  d = st.analysisDownload(id,'rh.white.obj');
 %}
 
 %%
+%{
+
+% I think there is a bug.
+
+% This session has an analysis.  But it is not returned in the search info
+session = st.search('session',...
+   'project label exact','Brain Beats',...
+   'session label exact','20180319_1232');
+
+% Here is the analysis as a search response.
+% Its session matches the id above
+
+
+analysisID = idGet(analysis{1},'data type','analysis')
+
+analysisOutput = st.fw.getAnalysis(analysisID);
+
+% How do w
+fname = analysisOutput.files{1}.name
+%}
+%{
+
+% From FlywheelExample.m
+% Have a look at the files
+% disp(cellfun(@(f) {f.name}, input_analysis.files))
+
+% Grab the segmented image
+% input_file = 'HERO_gka1_aparc.a2009s+aseg.nii.gz';
+% dest_path = fullfile('/scratch', input_file);
+% fw.downloadOutputFromAnalysis(input_analysis.id, input_file, dest_path);
+%}
 
 % We need to figure out how to download the whole analysis as a tar
 % file, or individual input and output files. Examples are in
 % FlywheelExamples.m
-disp('Analysis download is not yet implemented')
 
-end
-
-%{
 %% Parse inputs
 varargin = stParamFormat(varargin);
 
 p = inputParser;
 p.addRequired('id',@ischar);
-p.parse(id,varargin{:});
+p.addRequired('fname',@ischar);
+p.parse(id,fname,varargin{:});
+
+%% Download a file from analysis
+
+destination = fullfile(stRootPath,'local',fname);
+destination = obj.fw.downloadOutputFromAnalysis(id, fname, destination);
+
+end
+
+%{
 
 %% Make the flywheel sdk call
 
