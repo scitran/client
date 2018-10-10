a24a7fa97a91922033cc0d83b07a2d090d9572d6
Michael Perry <lmperry@stanford.edu>
2018-10-10 14:14:03 -0700

    Update default sdk install to latest version

--- utility/stFlywheelSDK.m
+++ utility/stFlywheelSDK.m
@@ -65,7 +65,7 @@ function [status, url, toolboxTable] = stFlywheelSDK(action,varargin)
 p = inputParser;
 varargin = stParamFormat(varargin);
 p.addRequired('action',@ischar);
-p.addParameter('sdkversion','2.5.0',@ischar);
+p.addParameter('sdkversion','4.1.0',@ischar);
 p.addParameter('summary',true,@islogical);
 
 p.parse(action,varargin{:});
