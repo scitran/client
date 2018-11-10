01aaae212f2f3146a4097b5940ac7ebafcdb0cc8
Brian Wandell <wandell@stanford.edu>
2018-11-10 14:58:00 -0800

    Cleaning

--- validate/v_stCheckDatabase.m
+++ validate/v_stCheckDatabase.m
@@ -1,5 +1,8 @@
 %% Check that the database is running 
 %
+%  Opens up a browser to the site.
+%  Prints to the screen a list of all the projects on the site
+%
 % LMP/BW Scitran Team, 2016
 
 %% Check that the database is up
