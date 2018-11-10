07134c0a6fcaf788722463840dfd864dff4215bb
Brian Wandell <wandell@stanford.edu>
2018-11-10 14:08:14 -0800

    Updated

--- validate/update/v_stCheckDatabase.m
+++ validate/update/v_stCheckDatabase.m
@@ -4,22 +4,13 @@
 
 %% Check that the database is up
 
-% This needs to be your specific instance
-url = 'https://flywheel.scitran.stanford.edu';
-% url = 'https://cni.stanford.edu'
+st = scitran('stanfordlabs');
 
-cmd = sprintf('curl -Is -k %s | grep HTTP | cut -d '' '' -f2', url);
-% cmd = sprintf('curl -Is %s', url)
+st.browser;
 
-[status, result] = system(cmd);
-result = strtrim(result);  % Strip space
+%%  Print the project labels
 
-% Result has a blank in it somewhere
-if strfind(result,'200') % then we're good
-    fprintf('Flywheel site detected at url %s\n',url);
-else
-    fprintf('Flywheel site not found\nurl tested:  %s\n',url);
-end
-
+projects = st.list('projects','all');
+stPrint(projects,'label');
 
 %%
