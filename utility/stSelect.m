b02c9d0fc50d959c6f1c2cda207c6a6522529cf8
Brian Wandell <wandell@stanford.edu>
2018-12-22 12:38:40 -0800

    Added a general stSelect for choosing sessions or acquisitions with a particular label.  Should work for files with a particular name, too.  Probably we should delete stFileSelect.m

--- utility/stSelect.m
+++ utility/stSelect.m
@@ -1,19 +1,19 @@
-function selectedFiles = stFileSelect(files,slot,matchVal,varargin)
-% Select files from a cell array of files based on a parameter
+function selected = stSelect(containers,slot,matchVal,varargin)
+% Select containers from a cell array of containers that match a parameter
 %
 % Synopsis
-%   selectedFiles = stFileSelect(files,slot,matchVal,varargin)
+%  selected = stSelect(container,slot,matchVal,varargin)
 %
 % Inputs
-%  files    - cell array of files from a scitran.list
-%  slot     - the slot that must be matched
-%  matchVal - String that the slot must match
+%  container - cell array of containers from a scitran.list
+%  slot      - the slot that must be matched
+%  matchVal  - String that the slot must match
 %
 % Optional key/value pairs
-%    info field name, the info field value
+%  info field name, the info field value
 %
 % Returns
-%  selectedFiles - The files that matched
+%  selected - The files that matched
 %
 %
 % ZL/Wandell Vistasoft 2018
@@ -23,10 +23,12 @@ function selectedFiles = stFileSelect(files,slot,matchVal,varargin)
 
 % Examples:
 %{
-  stFileSelect(files,'type','archive','asset','car');
+  stSelect(files,'type','archive','asset','car');
 %}
 %{
-  stFileSelect(files,'name',filename);
+%}
+%{
+  stSelect(files,'name',filename);
 %}
 
 %% Parse inputs
@@ -37,17 +39,17 @@ p.addRequired('slot',@ischar);
 p.addRequired('matchVal',@ischar);
 p.addParameter('infoval','',@ischar);
 
-p.parse(files,slot,matchVal); %,varargin{:});
+p.parse(containers,slot,matchVal); %,varargin{:});
 matchVal = ieParamFormat(matchVal);
 
 %%  Check if the file type and the critical info field matches the requirements
 
 cnt = 1;
-selectedFiles = {};
-for jj = 1:length(files)
+selected = {};
+for jj = 1:length(containers)
     % Force the slot value to lower case and no spaces.
     try
-        slotVal = ieParamFormat(files{jj}.(slot));
+        slotVal = ieParamFormat(containers{jj}.(slot));
     catch
         % If it doesn't exist, set it to empty.
         slotVal = '';
@@ -57,14 +59,14 @@ for jj = 1:length(files)
     if isequal(slotVal,matchVal)
         if isempty(varargin)
             % No info fields to check.
-            selectedFiles{cnt} = files{jj}; %#ok<AGROW>
+            selected{cnt} = containers{jj}; %#ok<AGROW>
             cnt = cnt + 1;
         else
             % There is also an info field specified.  Check that.
             try
                 % Does the info field match?  This must be exact.
-                if isequal(files{jj}.info.(varargin{1}),varargin{2})
-                    selectedFiles{cnt} = files{jj}; %#ok<AGROW>
+                if isequal(containers{jj}.info.(varargin{1}),varargin{2})
+                    selected{cnt} = containers{jj}; %#ok<AGROW>
                     cnt = cnt + 1;
                 end
             catch
