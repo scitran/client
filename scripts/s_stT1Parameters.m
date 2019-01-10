5944fec49047bc001b939b18320f156cfe619c81
Brian Wandell <wandell@stanford.edu>
2019-01-09 22:43:39 -0800

    Looking for info.  But ADNI is not a good choice because, well, it was not brought in through the scanner

--- scripts/s_stT1Parameters.m
+++ scripts/s_stT1Parameters.m
@@ -10,3 +10,9 @@ fileList =  st.search('file','file type','nifti',...
     'project label contains','T1',...
     'acquisition label contains','T1',...
     'summary', true);
+
+st.fw.lookup(
+id = st.objectParse(fileList{1});
+thisFile = st.list('file',fileList{1}.parent.id)
+stSelect(thisFile,'type','nifti')
+niftiFiles{1}.info
