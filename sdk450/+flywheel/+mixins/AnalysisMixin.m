44d96f51b6c6a4bde9fe603e9cff1ab79e01d3a8
wandell <wandell@stanford.edu>
2019-01-17 15:53:26 -0800

    Inserted the 450-Beta toolbox into this branch.

---
+++ sdk450/+flywheel/+mixins/AnalysisMixin.m
@@ -0,0 +1,18 @@
+classdef AnalysisMixin < flywheel.mixins.ContainerBase ...
+        & flywheel.mixins.TagMethods ...
+        & flywheel.mixins.InfoMethods ...
+        & flywheel.mixins.NoteMethods ...
+        & flywheel.mixins.FileMethods ...
+        & flywheel.mixins.DownloadMethods
+    properties
+        containerType_ = 'Analysis';
+    end
+    methods
+        function [returnData, resp] = uploadFile(obj, file)
+            [returnData, resp] = obj.invokeContainerApi('uploadOutputToAnalysis', obj.get('id'), file);
+        end
+        function [returnData, resp] = uploadOutput(obj, file)
+            [returnData, resp] = obj.invokeContainerApi('uploadOutputToAnalysis', obj.get('id'), file);
+        end
+    end
+end
---
+++ sdk450/README
@@ -0,0 +1,7 @@
+Matlab SDK Installation Instructions
+
+1. Unzip the flywheel-matlab-sdk zip file.
+2. Add flywheel-sdk/api/rest-client.jar to the java classpath
+    e.g. javaaddpath('flywheel-sdk/api/rest-client.jar')
+3. Add flywheel-sdk to the matlab path
+    e.g. addpath('flywheel-sdk')
