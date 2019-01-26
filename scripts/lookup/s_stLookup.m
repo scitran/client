5e3f3318215816a034631a18c20bb3f33f619daf
Brian Wandell <wandell@stanford.edu>
2019-01-25 16:07:38 -0800

    Updated Gear portion

--- scripts/lookup/s_stLookup.m
+++ scripts/lookup/s_stLookup.m
@@ -18,32 +18,44 @@ disp(myGroup)
 projects = st.search('project','group id','wandell','summary',true,'limit',3);
 pLabels = stPrint(projects,'project','label');
 
-lookupString = sprintf('%s/%s',myGroup.id,pLabels{1});
+lookupString = fullfile(myGroup.id,pLabels{1});
 project = st.lookup(lookupString);
 
 %% Now find the data for a subject in a project
 thisSession = project.sessions.findFirst();
 subject = thisSession.subject.code; % A Weston-Havens subject
-lookupString = sprintf('%s/%s/%s',myGroup.id,pLabels{1},subject);
+lookupString = fullfile(myGroup.id,pLabels{1},subject);
 
-st.lookup(lookupString);
+thisSubject = st.lookup(lookupString);
+disp(thisSubject)
 
 %% Next level
 sLabel = thisSession.label;
-lookupString = sprintf('%s/%s/%s/%s',myGroup.id,pLabels{1},subject,sLabel);
+lookupString = fullfile(myGroup.id,pLabels{1},subject,sLabel);
 thisSession2 = st.lookup(lookupString);
 assert(isequal(thisSession.id,thisSession2.id))
 
 %% Next level
 thisAcquisition = thisSession.acquisitions.findFirst;
 aLabel = thisAcquisition.label;
-lookupString = sprintf('%s/%s/%s/%s/%s',myGroup.id,pLabels{1},subject,sLabel,aLabel);
+lookupString = fullfile(myGroup.id,pLabels{1},subject,sLabel,aLabel);
 thisAcquisition2 = st.lookup(lookupString);
 assert(isequal(thisAcquisition2.id,thisAcquisition.id))
 
-%% Get a gear so you can run a job
+%% Look up all Gears.  This uses the 'name' field, not the label field.
 
 gears = st.fw.gears.find();
 gNames = stPrint(gears,'gear','name');
-str = fullfile('gears',gNames{1})
+idx = strcmp(gNames,'mriqc');
+gLabel = gears{idx}.gear.name;
+
+%%  Find the gear named 'mriqc'
+
+str = fullfile('gears',gLabel);
 thisGear = st.lookup(str);
+disp(thisGear)
+thisGear.gear
+
+%%
+
+
