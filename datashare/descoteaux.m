52184bf03ecaecfbf3b8458907c62249aba13b5d
wandell <wandell@stanford.edu>
2019-04-01 11:55:44 -0700

    Updated

--- datashare/descoteaux.m
+++ datashare/descoteaux.m
@@ -26,6 +26,48 @@ bval = st.search('files',...
 nFiles = numel(f);
 fprintf('Returned %d files\n',nFiles);
 
-outDir = fullfile(stRootPath,'local','descoteaux');
+outDir = fullfile('/scratch','descoteaux');
+if ~exist(outDir,'dir'), mkdir(outDir); end
+
 chdir(outDir);
+
+%%  Now, run the file download for all the files
+
+for ii=1:nFiles
+    
+    % Create a subdirectory labeled by session name
+    chdir(outDir)
+    thisFile = f{ii}.name;
+    s = split(thisFile,'.'); thisSession = fullfile(outDir,s{1});
+    if ~exist(thisSession,'dir'), mkdir(thisSession); end
+    chdir(thisSession);
+    
+    dest = fullfile(pwd,thisFile);
+    f{ii}.download(dest);
+    
+    thisBvec = bvec{ii}.name;
+    dest = fullfile(pwd,thisBvec);
+    bvec{ii}.download(dest);
+    
+    thisBval = bval{ii}.name;
+    dest = fullfile(pwd,thisBval);
+    bval{ii}.download(dest);
+    
+    fprintf('File %d\n',ii)
+
+end
+
+%% Then we zip it up and put it on Stanford Box
+%
+
+
+%%  We had to remove session 5327_14_1 because it is missing the bval.
+
+bvecNames = stPrint(bvec,'name');
+bvalNames = stPrint(bval,'name');
+for ii=1:length(bvecNames), [~,bvecNames{ii}] = fileparts(bvecNames{ii}); end 
+for ii=1:length(bvalNames), [~,bvalNames{ii}] = fileparts(bvalNames{ii}); end
+
+out = cellfun(@(x)(~contains(x,bvalNames)),bvecNames);
+bvecNames(out)
 
