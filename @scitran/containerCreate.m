d3604925ddeb055bd38544bab32e5d0ede3a7426
Brian Wandell <wandell@stanford.edu>
2019-05-15 12:59:20 -0700

    Fixed problem with checking for existing container before creating.  Updated containerDelete

--- @scitran/containerCreate.m
+++ @scitran/containerCreate.m
@@ -2,7 +2,7 @@ function idS = containerCreate(obj, group, project, varargin)
 % Create a project, session or acquisition on a Flywheel instance
 %
 % Syntax
-%   idS = st.containerCreate(groupL, projectL,...
+%   idS = st.containerCreate(groupLabel, projectLabel,...
 %                 'session',sessionLabel,...
 %                 'acquisition',acquisitionLabel)
 %
@@ -15,11 +15,11 @@ function idS = containerCreate(obj, group, project, varargin)
 %   There is a separate function (collectionCreate) for collections.
 %
 % Required Inputs
-%  groupL   - Group Label
-%  projectL - Project Label
+%  groupLabel   - Group Label
+%  projectLabel - Project Label
 %
-% Optional Parameters
-%  subject       - Make sure the project has a subject with this name 
+% Optional Parameters (thes4e are a
+%  subject       - Subject name
 %  session       - Session label
 %  acquisition   - Acquisition label
 %
@@ -29,8 +29,8 @@ function idS = containerCreate(obj, group, project, varargin)
 %
 % BW Scitran Team, 2016
 %
-% See also:  
-%  deleteContainer
+% See also:
+%  containerDelete
 %
 
 % Examples:
@@ -43,7 +43,13 @@ function idS = containerCreate(obj, group, project, varargin)
   id = st.containerCreate(gName, pLabel);
   status = st.exist('project',pLabel)
 
-  st.fw.deleteProject(id.project);
+  % Also testing containerDelete here
+  project = st.fw.get(id.project);
+  st.containerDelete(project,'query',true);
+
+  % Another way to do the deletion.  We use this below for session
+  % and acquisition
+  % st.fw.deleteProject(id.project);
 %}
 %{
   % Create a session within a project
@@ -53,12 +59,16 @@ function idS = containerCreate(obj, group, project, varargin)
   id = st.containerCreate(gName, pLabel,...
                 'subject',subject,...
                 'session',sLabel);
-  st.fw.deleteSession(id.session);
+
+  session = st.fw.get(id.session);
+  st.containerDelete(session);
+
+  % st.fw.deleteSession(id.session);
   st.fw.deleteProject(id.project);
 %}
 %{
   % Create an acquisition within a session within a project
-  % No subject for the session this time. 
+  % No subject for the session this time.
   pLabel = 'deleteSubject';
   sLabel = 'deleteSession';
   aLabel = 'deleteAcquisition'
@@ -66,15 +76,19 @@ function idS = containerCreate(obj, group, project, varargin)
        'session',sLabel,...
        'acquisition',aLabel);
 
-  st.fw.deleteAcquisition(id.acquisition);
+  acq = st.fw.get(id.acquisition);
+  st.containerDelete(acq);
+
+  % st.fw.deleteAcquisition(id.acquisition);
   st.fw.deleteSession(id.session);
   st.fw.deleteProject(id.project);
-
-  % When you create an acquisition, you can use the returned id to put a
-  % file, as in
-  id = st.create(gName, pLabel,'session',sLabel,'acquisition',aLabel);
-  st.uploadFile('file',filename,'id',id.acquisition);
-
+%}
+%{
+  % Create a new session within an existing project
+  st = scitran('stanfordlabs');
+  project = st.lookup('aldit/Recon Test');
+  id = st.containerCreate(project.group,project.label,'session','deleteMe');
+  st.fw.deleteSession(id.session);
 %}
 
 %% Input arguments are the project/session/acquisition labels
@@ -84,8 +98,8 @@ varargin = stParamFormat(varargin);
 p = inputParser;
 p.addRequired('group',@ischar);
 p.addRequired('project',@ischar);
+p.addParameter('subject','',@ischar);
 p.addParameter('session',[],@ischar);
-p.addParameter('subject',[],@ischar);
 
 p.addParameter('acquisition',[],@ischar);
 
@@ -97,9 +111,9 @@ p.parse(group,project,varargin{:});
 
 group       = p.Results.group;
 project     = p.Results.project;
+subject     = p.Results.subject;
 session     = p.Results.session;
 acquisition = p.Results.acquisition;
-subject     = p.Results.subject;
 
 % additionalData = p.Results.additionalData;  % NYI
 
@@ -113,7 +127,7 @@ end
 
 %% On to the project level
 
-% Does the project exist? 
+% Does the project exist?
 [status, idS.project] = obj.exist('project',project);
 if ~status
     % If not, add it.
@@ -121,45 +135,90 @@ if ~status
 end
 
 % Always get the project because we will probably need it either for the
-% subject or adding a session.  
+% subject or adding a session.
 project = obj.fw.get(idS.project);
 
 %% Did the user supply a subject or want one created?
 
-if isempty(subject) 
+subjectLabel = subject;
+if isempty(subjectLabel)
     % This is still the subject label.
     % User believes the subject is already part of the project, so empty.
     % But we don't know what subject to assign, so it will be the default,
     % which is 'unknown'.
     idS.subject = '';
 else
-    % Not empty, so try to find the subject.
-    str = sprintf('label=%s',subject);
-    subject = project.subjects.findOne(str);
-    if isempty(subject)
+    % Subject label exists, so try to find the subject.
+    str = sprintf('label=%s',subjectLabel);
+    try
+        subject = project.subjects.findOne(str);
+    catch
         % Not there, so create the subject for this project with this label
-        subject = project.addSubject('label',subject,'code',subject);
+        subject = project.addSubject('label',subjectLabel,'code',subject);
     end
+    
     % Add the subject ID to the outpu
     idS.subject = subject.id;
 end
 
 %% If no session is passed, then we are done and return the project ID
-if isempty(session), return; end
 
-% Create a new session for this subject.
+% Then, we check whether a session with the name 'session' already exists.
+% If it does, we return
+if isempty(session), return; end
+sessionLabel = session;
+str = sprintf('label=%s',sessionLabel);
+
+% The user passed a name and it does not exist.
+% Create a new session for this subject, or if no subject with a
+% default subject.
 if exist('subject','var') && ~isempty(subject)
-    session = subject.addSession('label',session);
+    % Subject exists
+    try
+        session = subject.sessions.findOne(str);
+    catch
+        session = subject.addSession('label',sessionLabel);
+    end
 else
-    session = project.addSession(struct('label', session, 'project', idS.project));
+    % No subject.  So find the session from within the project.
+    try
+        % This is a test if the session exists
+        session = project.sessions.findOne(str);
+    catch
+        % Create it from the project level
+        session = project.addSession(struct('label', sessionLabel, 'project', idS.project));
+        
+        % [status, idS.session] = obj.exist('session', session, 'parentID', idS.project);
+        % if ~status
+        % If not, add it. Should we check with the user?
+        %
+        % idS.session = obj.fw.addSession(struct('label', session, 'project', idS.project));
+        %{
+              % If the subject exists, we should also update the subject field when
+              % we create a session
+              if ~isempty(subjectLabel)
+                thisSession = st.fw.get(idS.session);
+                thisSubject = thisSession.subject;
+                thisSubject.update('label',subjectLabel);
+              end
+        %}
+        % end
+    end
 end
 
 idS.session = session.id;
 
 %% Test for the acquisition label; does it exist? If not create it
 if isempty(acquisition), return; end
+acquisitionLabel = acquisition;
+str = sprintf('label=%s',acquisitionLabel);
 
-acq = session.addAcquisition('label', acquisition);
+try
+    acq = session.acquisition.findOne(str);
+catch
+    acq = session.addAcquisition('label', acquisitionLabel);
+end
+
 idS.acquisition = acq.id;
 
 end
